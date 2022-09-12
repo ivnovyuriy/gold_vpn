e374aafc6dc07ad564fbbb2081ca98e24fc7a888
ivnovyuriy <ivanovyuriy@gmail.com>
2022-09-12 10:38:41 +0000

    Fix some bugs with EIP

--- eip_nat.tf
+++ eip_nat.tf
@@ -1,16 +1,22 @@
-# Elastic IP
-
-resource "aws_eip" "nat1" {
-  depends_on = [aws_internet_gateway.main-gw]
-  tags = {
-    Name = "EIP"
-  }
-
+resource "aws_eip" "my-eip" {
+  instance = aws_instance.vpn-server.id
+  vpc      = true
 }
+
+# # Elastic IP created in AWS web interface
+
+# data "aws_eip" "eip_by_filter" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["EIP"] # Name of EIP in web interface
+#   }
+# }
+
+# # Associate an EIP to an EC2 instance (creating by terraform)
 
 # resource "aws_eip_association" "eip_assoc" {
 #   instance_id   = aws_instance.vpn-server.id
-#   allocation_id = "eipalloc-0d24a776cbe8f60be"
+#   allocation_id = data.aws_eip.eip_by_filter.id
 # }
 
 # NAT GateWay
